7b4113f419c1d92be668e4e4d281481e51ec9bb2
simaorocha65234 <simaoptrocha@hotmail.com>
2018-12-20 17:38:16 +0000

    cancelar tarefa; set alarms; ajustar alarms; executar tarefa

--- server.c
+++ server.c
@@ -21,20 +21,28 @@ struct Tarefas
 {
     int id;
     char cmd[100];
-    char estado;
+    int state;
+    double timeToTask;
     int dia, mes, ano, hora, minuto;
     char output[1024];
 };
 
 struct Alarm
 {
-    int alarmID;
-    int timeToTask;
+    int taskID[100];
+    double timeToTask;
+    int ntasks;
+};
+
+struct Comands
+{
+    char cmd[100];
 };
 
 //Variáveis globais...
 struct Tarefas tarefa[200];
 struct Alarm alarms[200];
+struct Comands comandos[100];
 
 int countTarefas()
 {
@@ -57,10 +65,6 @@ int getAlarm()
 {
 }
 
-int alarmStatus(double difference)
-{
-}
-
 time_t getSeconds(int *dataValues[3], int *timeValues[2])
 {
 
@@ -74,7 +78,7 @@ time_t getSeconds(int *dataValues[3], int *timeValues[2])
     time(&timeB);
 
     timeinfo = localtime(&timeA);
-    printf("Current local time and date: %s", asctime(timeinfo));
+    // printf("Current local time and date: %s", asctime(timeinfo));
 
     tptr = localtime(&timeA);
     tA = *tptr;
@@ -84,12 +88,12 @@ time_t getSeconds(int *dataValues[3], int *timeValues[2])
     tA.tm_mon = tA.tm_mon + 1;
     tA.tm_year = (tA.tm_year + 1900);
 
-    printf("Tn\n");
-    printf("dia - %d\n", tA.tm_mday);
-    printf("mes - %d\n", tA.tm_mon);
-    printf("ano - %d\n", tA.tm_year);
-    printf("hor - %d\n", tA.tm_hour);
-    printf("min - %d\n", tA.tm_min);
+    // printf("Tn\n");
+    // printf("dia - %d\n", tA.tm_mday);
+    // printf("mes - %d\n", tA.tm_mon);
+    // printf("ano - %d\n", tA.tm_year);
+    // printf("hor - %d\n", tA.tm_hour);
+    // printf("min - %d\n", tA.tm_min);
 
     tB.tm_mday = dataValues[0];
     tB.tm_mon = dataValues[1];
@@ -97,31 +101,95 @@ time_t getSeconds(int *dataValues[3], int *timeValues[2])
     tB.tm_hour = timeValues[0];
     tB.tm_min = timeValues[1];
 
-    printf("Tn\n");
-    printf("dia - %d\n", tB.tm_mday);
-    printf("mes - %d\n", tB.tm_mon);
-    printf("ano - %d\n", tB.tm_year);
-    printf("hor - %d\n", tB.tm_hour);
-    printf("min - %d\n", tB.tm_min);
+    // printf("Tn\n");
+    // printf("dia - %d\n", tB.tm_mday);
+    // printf("mes - %d\n", tB.tm_mon);
+    // printf("ano - %d\n", tB.tm_year);
+    // printf("hor - %d\n", tB.tm_hour);
+    // printf("min - %d\n", tB.tm_min);
 
     timeA = mktime(&tA);
     timeB = mktime(&tB);
 
     difference = difftime(timeB, timeA);
-    // printf("Difference is %.0f seconds\n", difference);
+    printf("Difference is %.0f seconds\n", difference);
     return difference;
 }
-int setAlarm(timeToTask)
+int setAlarm()
 {
-    int timeToNext = getAlarm();
-    if (timeToNext > timeToTask)
+    int *dia, *mes, *ano, *hora, *minuto;
+    int *dataAux[3], *timeAux[2], taskAux[10];
+    double diffAux = 999999999999999999999999999;
+    int eq = 0;
+    int n = countTarefas();
+    for (int i = 0; i < n; i++)
     {
+        if (tarefa[i].state == 0)
+        {
+            dataAux[0] = tarefa[i].dia;
+            dataAux[1] = tarefa[i].mes;
+            dataAux[2] = tarefa[i].ano;
+            timeAux[0] = tarefa[i].hora;
+            timeAux[1] = tarefa[i].minuto;
 
-        //CRIAR ALARME
+            double difference = getSeconds(dataAux, timeAux);
+
+            tarefa[i].timeToTask = difference;
+            if (difference < diffAux)
+            {
+                diffAux = difference;
+            }
+        }
+    }
+
+    alarms[0].timeToTask = diffAux;
+
+    int aux = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (tarefa[i].timeToTask == diffAux)
+        {
+            alarms[0].taskID[aux] = tarefa[i].id;
+            aux++;
+        }
+    }
+    alarms[0].ntasks = aux;
+    if (aux == 1)
+    {
+        printf("Proxima tarefa %d em  %.0f seconds\n", alarms[0].taskID[0], alarms[0].timeToTask);
+    }
+    else
+    {
+        printf("\n Numero de tarefas agendadas para o mesmo instante: %d\n", aux);
+        printf("Tarefas:\n");
+        for (int i = 0; i < aux; i++)
+        {
+            printf("%d\n", alarms[0].taskID[i]);
+        }
+        printf("em %.0f segundos", alarms[0].timeToTask);
+    }
+    alarm(alarms[0].timeToTask);
+    return 0;
+}
+
+int cancelTask(int taskID)
+{
+    int n = countTarefas();
+    for (int i = 0; i < n; i++)
+    {
+        if (tarefa[i].id == taskID)
+        {
+            tarefa[i].state = 2; // 2 para cancelado
+            // n = countAlarms();
+            // for(i=0; i< n ; i++){
+            //     if(alarms[i].taskID == taskID)
+
+            // }
+        }
     }
 }
 
-int execTask(int taskID)
+int execTask()
 {
 
     int nbyte = 256;
@@ -129,13 +197,33 @@ int execTask(int taskID)
     char *cmd[256], *token[nbyte];
     printf("\nEXEC TASK\n");
 
-    int n = countTarefas();
-    for (int i = 0; i < n; i++)
+    int *taskID[100];
+    int countCmd = alarms[0].ntasks;
+
+    for (int i = 0; i < countCmd; i++)
     {
-        if (tarefa[i].id == taskID)
-            strcpy(cmd, tarefa[i].cmd);
+        taskID[i] = alarms[0].taskID[i];
     }
-    printf("comando a executrar: %s\n", &cmd);
+
+    int n = countTarefas();
+
+    for (int aux = 0; aux < countCmd; aux++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (tarefa[i].id == taskID[aux])
+            {
+                strcpy(cmd, tarefa[i].cmd);
+                strcpy(comandos[aux].cmd, cmd);
+            }
+        }
+    }
+
+    for(int i =0; i<countCmd; i++){
+          printf("comando a executrar: %s\n", comandos[i].cmd);
+    }
+
+    printf("comando a executrar: %s\n", comandos[0].cmd);
 
     token[0] = strtok(cmd, delim);
     int i = 0;
@@ -151,7 +239,7 @@ int execTask(int taskID)
 
     int pid;
     pid = fork();
-    
+
     if (pid == 0)
     {
         execvp(token[0], token);
@@ -165,37 +253,42 @@ int execTask(int taskID)
     return 0;
 }
 
-// void signalHandler(int sig){
-//     int pid;
-//     switch (sig){
-//         case 1:
-//             printf("Cliente terminou conexão.\n");
-//             break;
-//         case 14:
-//             dprintf(1,"Alarme recebido\n");
-//             if((pid = fork()) < 0){
-//                 perror("fork no alarme");
-//                 exit(-1);
-//             } else if(pid == 0){
-//                 executarTarefa();
-//             } else {
-//                 wait(&pid);
-//             }
-//             ajustarAlarmes();
-//             break;
-//         default:
-//             printf("Sinal %d recebido\n",sig);
-//     }
-// }
+void signalhandler(int sig)
+{
+    int pid;
+    switch (sig)
+    {
+    case 1:
+        printf("Cliente terminou conexão.\n");
+        break;
+    case 14:
+        dprintf(1, "Alarme recebido\n");
+        if ((pid = fork()) < 0)
+        {
+            perror("fork no alarme");
+            exit(-1);
+        }
+        else if (pid == 0)
+        {
+            execTask();
+        }
+        else
+        {
+            wait(&pid);
+        }
+        break;
+    default:
+        printf("Sinal %d recebido\n", sig);
+    }
+}
 
 int main()
 {
 
     int fd1, nbyte = 256;
     char *buf1 = (char *)malloc(nbyte * sizeof(char)), *buf2 = (char *)malloc(nbyte * sizeof(char));
-    ;
     char delim[] = " ";
-    char *agendar = "-a", *listar = "-l", *exec = "-e";
+    char *agendar = "-a", *listar = "-l", *exec = "-e", *cancelar = "-c";
     int len;
     char *data[10];
     char *horas[10];
@@ -216,6 +309,7 @@ int main()
     // Creating the named file(FIFO)
     // mkfifo(<pathname>,<permission>)
     mkfifo(myfifo, 0666);
+    signal(SIGALRM, signalhandler);
 
     char *token[nbyte], *dataToken[nbyte], *timeToken[100], response[256];
     while (1)
@@ -282,7 +376,7 @@ int main()
             timeAux[1] = minuto;
 
             double difference = getSeconds(dataAux, timeAux);
-            printf("Difference is %.0f seconds\n", difference);
+            // printf("Difference is %.0f seconds\n", difference);
 
             int n = countTarefas();
             printf("\nnumero de tarefas: \t %d\n", n);
@@ -294,8 +388,11 @@ int main()
             tarefa[n].ano = ano;
             tarefa[n].hora = hora;
             tarefa[n].minuto = minuto;
+            tarefa[n].timeToTask = difference;
+            tarefa[n].state = 0;
             strcpy(tarefa[n].cmd, comando);
 
+            setAlarm();
             // timeToTask = getSeconds(dataAux, timeAux);
 
             // storeTask(tarefa[n].id, tarefa[n].data, tarefa[n].hora);
@@ -303,18 +400,35 @@ int main()
         // listar
         else if (strcmp(token[0], listar) == 0)
         {
+            char *printState;
             printf("Pedido de listagem\n");
             num = countTarefas();
             for (int i = 0; i < num; i++)
             {
-                printf("tafera: \t %d\n data : \t %d/%d/%d\n horas: \t %d:%d:00\n comando: %s\n", tarefa[i].id, tarefa[i].dia, tarefa[i].mes, tarefa[i].ano, tarefa[i].hora, tarefa[i].minuto, tarefa[i].cmd);
+                if (tarefa[i].state == 0)
+                    printState = "Agendada";
+                else if (tarefa[i].state == 1)
+                    printState = "Efectuada";
+                else if (tarefa[i].state == 2)
+                    printState = "Cancelada";
+
+                printf("tafera: \t %d\n data : \t %d/%d/%d\n horas: \t %d:%d:00\n estado: %s\n comando: %s\n", tarefa[i].id, tarefa[i].dia, tarefa[i].mes, tarefa[i].ano, tarefa[i].hora, tarefa[i].minuto, printState, tarefa[i].cmd);
             }
         }
 
         else if ((strcmp(token[0], exec) == 0))
         {
             printf("Pedido para exec\n");
-            execTask(0);
+            setAlarm();
+        }
+
+        else if ((strcmp(token[0], cancelar) == 0))
+        {
+            int idTarefa;
+            printf("Pedido para cancelar\n");
+            sscanf(token[1], "%d\n", &idTarefa);
+            cancelTask(idTarefa);
+            printf("tarefa cancelada");
         }
 
         memset(buf1, 0, strlen(buf1));
